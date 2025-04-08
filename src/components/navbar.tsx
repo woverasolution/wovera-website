2d0740f3073e913d1667728eccbfd55d0e3dd72c
tconceptt <tendalamaw@gmail.com>
2025-04-08 11:53:48 +0300

    lints

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,7 +13,6 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import {
   ChevronRight,
   Home,
-  Info,
   Menu,
   Package,
   PhoneCall,
@@ -191,9 +190,7 @@ const Navbar = () => {
                 </ul>
               </NavigationMenuContent>
             </NavigationMenuItem>
-            <NavigationMenuItem>
-              
-            </NavigationMenuItem>
+            
             <NavigationMenuItem>
               <Link href="/contact" legacyBehavior passHref>
                 <NavigationMenuLink className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
